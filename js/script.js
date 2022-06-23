dea0a30f44093df94a79c1d105a6e3bc7d823411
Marcus G. Oware <90935734+Weten-Ogen@users.noreply.github.com>
2022-06-23 22:30:17 +0000

    end of today's section

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*==========typing animation ====*/
+
+var typed = new Typed(".typing",{
+    string:["JavaScript Developer", "Web Developer ", "React Developer", "Web Designer", "Problem Solver"],
+    typeSpeed: 100,
+    backSpeed:60,
+    loop:true
+});
