81a6389993d766078c7681999017322132fa643f
Marcus G. Oware <90935734+Weten-Ogen@users.noreply.github.com>
2022-06-23 16:59:38 +0000

    I am finally down with the essentials of my portfolio page

---
+++ js/style-switcher.js
@@ -0,0 +1,9 @@
+/* =============================toogle style switcher=============*/
+
+const styleSwitcherToggle =document.querySelector('.style-switcher-toggler');
+
+
+styleSwitcherToggle.addEventListener('click',() =>{
+    document.querySelector('.style-switcher').classList.toggle('open');
+
+})
